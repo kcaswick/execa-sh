fdc28e8abcb30b17cb4c7c17952f9995da726b5f
Kevin C <lablua@gmail.com>
2023-10-01 00:47:34 -0500

    Switch to vitest - test passes now

--- lib/__tests__/execaSh.test.ts
+++ lib/__tests__/execaSh.test.ts
@@ -1,10 +1,11 @@
-import assert from "assert";
+import { describe, expect, it } from "vitest";
+
 import { execaSh } from "../index.js";
 
 describe("execaSh", () => {
   it("has a test", () => {
     const helloString = "Hello, world!";
     const result = execaSh`echo ${helloString}`;
-    expect(result).resolves.toEqual(helloString);
+    expect(result).resolves.toHaveProperty("stdout", helloString);
   });
 });
